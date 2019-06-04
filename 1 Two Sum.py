687604da5de04721e4d57f3a12547563e79fca95
RayHHe <hh2676@columbia.edu>
2019-06-04 10:48:40 -0500

    Create 1 Two Sum.py
    How to use Class in python.

---
+++ 1 Two Sum.py
@@ -0,0 +1,7 @@
+# No.1/2019-06-04/5240 ms/13.8 MB
+class Solution:
+    def twoSum(self, nums, target):
+        for i in range(0,len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[i]+nums[j]==target:
+                    return [i,j]
