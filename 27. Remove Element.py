ef6f933a1424a1f027b577485d99592bdee7d8d4
RayHHe <hh2676@columbia.edu>
2019-06-12 11:08:16 -0500

    Create 27. Remove Element.py

---
+++ 27. Remove Element.py
@@ -0,0 +1,9 @@
+# No.1/2019-06-12/36 ms/13.1 MB
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        while 1:
+            try:
+                nums.remove(val)
+            except: break
+                
